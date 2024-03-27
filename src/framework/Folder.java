494ee3edec987e54c6eccadaffc2ff9a0ef86064
catchacats <164878232+catchacats@users.noreply.github.com>
2024-03-27 22:19:33 +0800

    Folder.java

--- src/framework/Folder.java
+++ src/framework/Folder.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.List;  
   
 public class Folder extends FolderItem {  
-	private List<FolderItem> folderItems;  
+	private List<FolderItem> folderItems;
+	private int totalSize;
   
 	public Folder(String initialName) {  
 		super(initialName, isFolder());  
